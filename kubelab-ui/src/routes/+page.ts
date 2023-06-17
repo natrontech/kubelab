3c0bdb09a2f9bde9cb6b0d19093ef6949f8ec2a1
Jan Lauber <jan.lauber@protonmail.ch>
2023-06-17 16:07:45 +0200

    fix: login
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- kubelab-ui/src/routes/+page.ts
+++ kubelab-ui/src/routes/+page.ts
@@ -1,9 +1,16 @@
 import { updateDataStores } from "$lib/stores/data";
 import toast from "svelte-french-toast";
 import type { PageLoad } from "./$types";
+import { browser } from "$app/environment";
+import { client } from "$lib/pocketbase";
 
 export const load: PageLoad = async () => {
-    await updateDataStores().catch((error) => {
-        toast.error(error);
-    });
+
+    if (browser) {
+        if (client.authStore.model) {
+            await updateDataStores().catch((error) => {
+                toast.error(error);
+            });
+        }
+    }
 };
