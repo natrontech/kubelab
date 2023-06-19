714b822f5c21197268c7129575f91563ce9c78a1
Jan Lauber <jan.lauber@protonmail.ch>
2023-06-19 21:41:37 +0200

    fix: back to labs loading exercises bug
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- kubelab-ui/src/routes/labs/[id]/+page.svelte
+++ kubelab-ui/src/routes/labs/[id]/+page.svelte
@@ -11,12 +11,23 @@
   import { client } from "$lib/pocketbase";
   import toast from "svelte-french-toast";
   import { loadingExercises } from "$lib/stores/loading";
+  import { onDestroy, onMount } from "svelte";
 
   $metadata.title = "Exercises";
 
   function isExerciseRunning(exercise_id: string) {
     return getExerciseSessionByExercise(exercise_id)?.agentRunning;
   }
+
+  let show = false;
+
+  onMount(() => {
+    show = true;
+  });
+
+  onDestroy(() => {
+    show = false;
+  });
 
   async function startExercise(exercise_id: string) {
     const data: ExerciseSessionsRecord = {
@@ -61,57 +72,59 @@
   }
 </script>
 
-<a class="btn top-5 absolute" href="/labs/">
+<a class="btn top-5 absolute" href="/labs/" on:click={() => show = false}>
   <ArrowLeft class="inline-block w-4 h-4 mr-2" />
   Labs
 </a>
 <h1 class="text-center text-4xl font-bold my-4">Exercises</h1>
 <div class="grid grid-cols-3 gap-4">
-  {#key $exercise_sessions}
-    {#each $exercises as exercise, i}
-      <div
-        class="card w-full {getExerciseSessionByExercise(exercise.id)?.endTime
-          ? 'bg-green-200'
-          : 'bg-base-200'} border-4 border-black"
-      >
-        <div class="card-body">
-          <p class="badge badge-neutral absolute top-2 right-2">#{i + 1}</p>
-          <p
-            class="badge {getExerciseSessionByExercise(exercise.id)?.agentRunning
-              ? 'badge-success'
-              : 'badge-error'} absolute top-2 left-2"
-          >
-            {getExerciseSessionByExercise(exercise.id)?.agentRunning ? "Running" : "Stopped"}
-          </p>
-          <p
-            class="badge badge-outline {getExerciseSessionByExercise(exercise.id)?.endTime
-              ? 'badge-neutral'
-              : 'badge-error'} absolute bottom-2 left-2"
-          >
-            {getExerciseSessionByExercise(exercise.id)?.endTime ? "Completed" : "Not Completed"}
-          </p>
-          <h2 class="card-title mt-2">{exercise.title}</h2>
-          <div class="flex gap-2 justify-end">
-            <div class="tooltip" data-tip="start exercise">
-              <button
-                class="btn {isExerciseRunning(exercise.id) ? 'btn-disabled' : 'btn-neutral'}"
-                on:click={() => startExercise(exercise.id)}
-              >
-                {#if $loadingExercises.includes(exercise.id)}
-                  <span class="loading loading-dots loading-md" />
-                {:else}
-                  <Play />
-                {/if}
-              </button>
-            </div>
-            <a href={exercise.id} class="card-actions justify-end cursor-pointer">
-              <div class="tooltip" data-tip="open console">
-                <button class="btn btn-neutral"><TerminalSquare /></button>
+  {#if show}
+    {#key $exercise_sessions}
+      {#each $exercises as exercise, i}
+        <div
+          class="card w-full {getExerciseSessionByExercise(exercise.id)?.endTime
+            ? 'bg-green-200'
+            : 'bg-base-200'} border-4 border-black"
+        >
+          <div class="card-body">
+            <p class="badge badge-neutral absolute top-2 right-2">#{i + 1}</p>
+            <p
+              class="badge {getExerciseSessionByExercise(exercise.id)?.agentRunning
+                ? 'badge-success'
+                : 'badge-error'} absolute top-2 left-2"
+            >
+              {getExerciseSessionByExercise(exercise.id)?.agentRunning ? "Running" : "Stopped"}
+            </p>
+            <p
+              class="badge badge-outline {getExerciseSessionByExercise(exercise.id)?.endTime
+                ? 'badge-neutral'
+                : 'badge-error'} absolute bottom-2 left-2"
+            >
+              {getExerciseSessionByExercise(exercise.id)?.endTime ? "Completed" : "Not Completed"}
+            </p>
+            <h2 class="card-title mt-2">{exercise.title}</h2>
+            <div class="flex gap-2 justify-end">
+              <div class="tooltip" data-tip="start exercise">
+                <button
+                  class="btn {isExerciseRunning(exercise.id) ? 'btn-disabled' : 'btn-neutral'}"
+                  on:click={() => startExercise(exercise.id)}
+                >
+                  {#if $loadingExercises.includes(exercise.id)}
+                    <span class="loading loading-dots loading-md" />
+                  {:else}
+                    <Play />
+                  {/if}
+                </button>
               </div>
-            </a>
+              <a href={exercise.id} class="card-actions justify-end cursor-pointer">
+                <div class="tooltip" data-tip="open console">
+                  <button class="btn btn-neutral"><TerminalSquare /></button>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    {/each}
-  {/key}
+      {/each}
+    {/key}
+  {/if}
 </div>
