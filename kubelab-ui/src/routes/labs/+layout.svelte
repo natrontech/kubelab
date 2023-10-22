235f3091450be9b1f40410a1c2ab315a9f2f0e8b
Jan Lauber <jan.lauber@protonmail.ch>
2023-10-22 21:57:32 +0200

    fix: scroll issue at labs
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- kubelab-ui/src/routes/labs/+layout.svelte
+++ kubelab-ui/src/routes/labs/+layout.svelte
@@ -1,5 +1,5 @@
-<div class="absolute top-16 bottom-0 right-0 left-0 overflow-y-scroll scrollbar-none
-bg-gradient-to-r from-blue-500 to-purple-500 dark:from-base-100 dark:to-base-100
-">
+<div
+  class="absolute top-16 bottom-0 right-0 left-0 overflow-hidden bg-gradient-to-r from-blue-500 to-purple-500 dark:from-base-100 dark:to-base-100"
+>
   <slot />
 </div>
